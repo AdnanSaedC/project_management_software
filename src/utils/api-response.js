9740ae947f2e7d4153b0582d974b38818ebf49a6
AdnanSaedC <adnansaed180@gmail.com>
2025-12-07 00:32:25 +0530

    apiresponse got updated
    Add comments to clarify the purpose and usage of the ApiResponse class.

--- src/utils/api-response.js
+++ src/utils/api-response.js
@@ -1,6 +1,9 @@
 //we are trying top create a standard api response format for our application
 //here api is nothing but the format of response from our server to the client
 
+//here constructor is nothing just it sets the initial values
+//when you are creating anobject of it you ahve to pass these values
+
 class ApiResponse {
     constructor(StatusCode, data, message = "Success") {
         this.statusCode = StatusCode;
